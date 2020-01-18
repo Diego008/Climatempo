8e6a2c70d7ca1dd4fc8d6d3f683730846ef6f92d
Diego008 <diegoaugusto008@gmail.com>
2020-01-18 12:53:53 -0300

    adicionando conteudo da previsao

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = process.env.PORT || 5000
+const routes = require('./routes.js');
 
 //app.use(express.json()) //Passando para aplicação que irá utilizar o JSON
 app.set('view engine', 'ejs') //Setamos que nossa engine será o ejs;
@@ -15,10 +16,6 @@ app.listen(port, () => {
     console.log(`Executar projeto em: http://localhost:${port}`)
 })
 
-app.get('/', (req, res) => {
-    res.render('pages/home')
-})
-
 //middleware para verificar se a requisição é o favicon.ico, se true, retorna vazio invés do retorno 404
 
 app.use((req, res, next) => {
@@ -29,3 +26,5 @@ app.use((req, res, next) => {
         next();
     }
 });
+
+app.use(routes);
